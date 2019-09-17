c930dbd636e978ddef94629c03f5aea14e4801ba
lipatov <triton01@inbox.ru>
2019-09-17 19:02:25 +0300

    'JavaSort' challenge is done!

--- src/DataStructure/JavaSort/Solution.java
+++ src/DataStructure/JavaSort/Solution.java
@@ -2,36 +2,40 @@ package DataStructure.JavaSort;
 
 import java.util.*;
 
-class Student{
+class Student {
+
     private int id;
     private String fname;
     private double cgpa;
+
     public Student(int id, String fname, double cgpa) {
         super();
         this.id = id;
         this.fname = fname;
         this.cgpa = cgpa;
     }
+
     public int getId() {
         return id;
     }
+
     public String getFname() {
         return fname;
     }
+
     public double getCgpa() {
         return cgpa;
     }
+
 }
 
-//Complete the code
-public class Solution
-{
-    public static void main(String[] args){
+public class Solution {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int testCases = Integer.parseInt(in.nextLine());
 
-        List<Student> studentList = new ArrayList<Student>();
-        while(testCases>0){
+        List<Student> studentList = new ArrayList<>();
+        while (testCases > 0) {
             int id = in.nextInt();
             String fname = in.next();
             double cgpa = in.nextDouble();
@@ -42,7 +46,15 @@ public class Solution
             testCases--;
         }
 
-        for(Student st: studentList){
+        studentList.sort((c1, c2) -> {
+            if (Double.compare(c2.getCgpa(), c1.getCgpa()) != 0)
+                return Double.compare(c2.getCgpa(), c1.getCgpa());
+            else if (Double.compare(c2.getCgpa(), c1.getCgpa()) == 0 && c2.getFname().compareTo(c1.getFname()) != 0)
+                return c1.getFname().compareTo(c2.getFname());
+            return Integer.compare(c2.getId(), c1.getId());
+        });
+
+        for (Student st : studentList) {
             System.out.println(st.getFname());
         }
     }
