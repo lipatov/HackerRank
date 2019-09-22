d8bb2f3301fe3ae74cc0ac42e292492ff862ac10
lipatov <triton01@inbox.ru>
2019-09-22 22:01:28 +0300

    'JavaMethodOverriding2' challenge is done!

---
+++ src/DataStructure/OOP/JavaMethodOverriding2/Solution.java
@@ -0,0 +1,27 @@
+package DataStructure.OOP.JavaMethodOverriding2;
+
+class BiCycle {
+    String define_me() {
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle {
+    MotorCycle() {
+        System.out.println("Hello I am a motorcycle, I am " + define_me());
+
+        String temp = super.define_me(); //Fix this line
+
+        System.out.println("My ancestor is a cycle who is " + temp);
+    }
+
+    String define_me() {
+        return "a cycle with an engine.";
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        MotorCycle M = new MotorCycle();
+    }
+}
