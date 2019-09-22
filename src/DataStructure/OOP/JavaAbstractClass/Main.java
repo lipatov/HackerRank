56f1f64c008d8693ddd6e3e315120bc1097b189b
lipatov <triton01@inbox.ru>
2019-09-22 21:40:56 +0300

    'JavaAbstractClass' challenge is done!

---
+++ src/DataStructure/OOP/JavaAbstractClass/Main.java
@@ -0,0 +1,33 @@
+package DataStructure.OOP.JavaAbstractClass;
+
+import java.util.Scanner;
+
+abstract class Book {
+    String title;
+
+    String getTitle() {
+        return title;
+    }
+
+    abstract void setTitle(String s);
+}
+
+class MyBook extends Book {
+    String title;
+
+    @Override
+    public void setTitle(String title) {
+        super.title = title;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+        sc.close();
+    }
+}
